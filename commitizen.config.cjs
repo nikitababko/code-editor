def6eb901d28e4888e70d7564b288281c2e37d4d
Nikita Babko <niki.babko@gmail.com>
2025-10-26 15:31:43 +0700

    chore(commitizen): replace [''] with [] in scopes config

--- commitizen.config.cjs
+++ commitizen.config.cjs
@@ -45,7 +45,7 @@ module.exports = {
       name: 'revert:    Reverts a previous commit',
     },
   ],
-  scopes: [''],
+  scopes: [],
   allowCustomScopes: true,
   allowBreakingChanges: false,
 };
